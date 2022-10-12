a4a8ec8832b8d6cf1c60c06114cfe4bd6ba9a8da
Tao Hansen <53713149+taohansens@users.noreply.github.com>
2022-10-12 12:39:25 -0300

    First module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,4 @@
+import { NewTransferComponent } from './new-transfer/new-transfer.component';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
@@ -5,7 +6,8 @@ import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NewTransferComponent
   ],
   imports: [
     BrowserModule
