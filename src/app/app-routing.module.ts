c26eb49b54d0c77bb9c96b898b14d1c2a3e0082b
Tao Hansen <53713149+taohansens@users.noreply.github.com>
2022-10-13 01:41:21 -0300

    Router

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,18 @@
+import { ExtractComponent } from './extract/extract.component';
+import { NgModule } from "@angular/core";
+import { RouterModule } from "@angular/router";
+
+import { Routes } from '@angular/router';
+import { NewTransferComponent } from './new-transfer/new-transfer.component';
+
+export const routes: Routes = [
+  {path: '', redirectTo: 'extract', pathMatch: 'full'},
+  {path: 'extract', component: ExtractComponent},
+  {path: 'new-transfer', component: NewTransferComponent},
+]
+
+@NgModule(
+  {imports: [RouterModule.forRoot(routes)],
+   exports: [RouterModule]})
+
+export class AppRoutingModule{}
